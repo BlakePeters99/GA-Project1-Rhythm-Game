c905124f895d960c846a830aa121264702bd8d56
Blake Peters <blakerdpeters@gmail.com>
2024-10-29 16:32:19 -0700

    start and stop buttons working for both songs

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-let sound1 = document.getElementById('#audio1');
-let bodyElement = document.querySelector('body');
-let button1 = document.querySelector('.btn1');
+// let sound1 = document.getElementById('#audio1');
+// let bodyElement = document.querySelector('body');
+// let button1 = document.querySelector('.btn1');
 
-button1.addEventListener("click", click1());
+// button1.addEventListener("click", click1());
 
-function click1() {
-    sound1.onplay();
-    bodyElement.style.backgroundColor = 'red';
-}
-bodyElement.style.backgroundColor = 'blue';
+// function click1() {
+//     sound1.onplay();
+//     bodyElement.style.backgroundColor = 'red';
+// }
+// bodyElement.style.backgroundColor = 'blue';
