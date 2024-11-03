57fe072ba32bd5daa361cec65411dae740262d22
Blake Peters <blakerdpeters@gmail.com>
2024-11-03 13:26:11 -0800

    created flex container for button imgs

--- app.js
+++ app.js
@@ -7,39 +7,48 @@ let sound6 = document.getElementById('audio6');
 let sound7 = document.getElementById('audio7');
 let drum = document.getElementById("drum");
 let songNav = document.getElementById("songNav");
+let currentSong = 1;
 
 function startSong(num) {
     console.log(num);
+    stopSong();
     let sound;
     switch (num){
         case 1:
             sound = sound1
+            currentSong = 1;
+            sound.currentTime = 0;
+            break;
+            case 2: 
+            sound = sound2
             // Skips long intro
             sound.currentTime = 17;
+            currentSong = 2;
             break;
-        case 2: 
-            sound = sound2
-            sound.currentTime = 0;
-            break;
-        case 3: 
+            case 3: 
             sound = sound3
             sound.currentTime = 0;
+            currentSong = 3;
             break;
-        case 4: 
+            case 4: 
             sound = sound4
             sound.currentTime = 0;
+            currentSong = 4;
             break;
-        case 5: 
+            case 5: 
             sound = sound5
             sound.currentTime = 0;
+            currentSong = 5;
             break;
-        case 6: 
+            case 6: 
             sound = sound6
             sound.currentTime = 0;
+            currentSong = 6;
             break;
-        case 7: 
+            case 7: 
             sound = sound7
             sound.currentTime = 0;
+            currentSong = 7;
             break;
 
     }
@@ -49,9 +58,9 @@ function startSong(num) {
     songNav.style.backgroundColor = 'green';
 }
 
-function stopSong(num) {
+function stopSong() {
     let sound;
-    switch(num){
+    switch(currentSong){
         case 1:
             sound = sound1;
             break;
