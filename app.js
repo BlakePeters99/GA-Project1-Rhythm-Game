26dc1aeba2c905ee8bac9200ba2ace4350ed59a1
Blake Peters <blakerdpeters@gmail.com>
2024-11-03 12:49:05 -0800

    config 7 songs

--- app.js
+++ app.js
@@ -1,19 +1,47 @@
 let sound1 = document.getElementById('audio1');
 let sound2 = document.getElementById('audio2');
+let sound3 = document.getElementById('audio3');
+let sound4 = document.getElementById('audio4');
+let sound5 = document.getElementById('audio5');
+let sound6 = document.getElementById('audio6');
+let sound7 = document.getElementById('audio7');
 let drum = document.getElementById("drum");
 let songNav = document.getElementById("songNav");
 
 function startSong(num) {
     console.log(num);
     let sound;
-    if (num === 1) {
-        sound = sound1
-        // Skips long intro
-        sound.currentTime = 17;
-    }
-    else {
-        sound = sound2
-        sound.currentTime = 0;
+    switch (num){
+        case 1:
+            sound = sound1
+            // Skips long intro
+            sound.currentTime = 17;
+            break;
+        case 2: 
+            sound = sound2
+            sound.currentTime = 0;
+            break;
+        case 3: 
+            sound = sound3
+            sound.currentTime = 0;
+            break;
+        case 4: 
+            sound = sound4
+            sound.currentTime = 0;
+            break;
+        case 5: 
+            sound = sound5
+            sound.currentTime = 0;
+            break;
+        case 6: 
+            sound = sound6
+            sound.currentTime = 0;
+            break;
+        case 7: 
+            sound = sound7
+            sound.currentTime = 0;
+            break;
+
     }
 
     sound.volume = 0.3;
@@ -23,11 +51,28 @@ function startSong(num) {
 
 function stopSong(num) {
     let sound;
-    if (num === 1) {
-        sound = sound1;
-    }
-    else {
-        sound = sound2;
+    switch(num){
+        case 1:
+            sound = sound1;
+            break;
+        case 2:
+            sound = sound2;
+            break;
+        case 3:
+            sound = sound3;
+            break;
+        case 4:
+            sound = sound4;
+            break;
+        case 5:
+            sound = sound5;
+            break;
+        case 6:
+            sound = sound6;
+            break;
+        case 7:
+            sound = sound7;
+            break;
     }
     sound.pause();
     songNav.style.backgroundColor = 'red';
