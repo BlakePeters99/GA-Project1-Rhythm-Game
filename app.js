91e75765def0ff5d43ca66f5bb48a5e0dc7a2fa1
Blake Peters <blakerdpeters@gmail.com>
2024-11-03 18:24:27 -0800

    fixed button timing and added locked feature to gifs

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ let b1 = document.getElementById('b1');
 let b2 = document.getElementById('b2');
 let b3 = document.getElementById('b3');
 let b4 = document.getElementById('b4');
+let img1 = document.getElementById('img1');
+let img2 = document.getElementById('img2');
+let img3 = document.getElementById('img3');
+let img4 = document.getElementById('img4');
+let lockedImg1 = document.getElementById('lockedImg1');
+let lockedImg2 = document.getElementById('lockedImg2');
+let lockedImg3 = document.getElementById('lockedImg3');
+let lockedImg4 = document.getElementById('lockedImg4');
+let lockedText1 = document.getElementById('lockedText1');
+let lockedText2 = document.getElementById('lockedText2');
+let lockedText3 = document.getElementById('lockedText3');
+let lockedText4 = document.getElementById('lockedText4');
 let sound1 = document.getElementById('audio1');
 let sound2 = document.getElementById('audio2');
 let sound3 = document.getElementById('audio3');
@@ -27,53 +39,68 @@ function startSong(num) {
     stopSong();
     // Resets button counter 
     buttonCounter = 0;
-    counter.innerText = `Beats Hit: ${buttonCounter}`
+    counter.innerText = `Beats Hit: ${buttonCounter}   `
     // Delays starting of new song so recursion processes stop
     setTimeout(() => {
-        let sound;
+        let sound, time;
 
         switch (num) {
             case 1:
                 sound = sound1
                 currentSong = 1;
                 sound.currentTime = 0;
+                sound.volume = 0.3;
+                time = 480;
+                
                 break;
             case 2:
                 sound = sound2
                 // Skips long intro
                 sound.currentTime = 19.5;
                 currentSong = 2;
+                sound.volume = 0.3;
+                time = 2000;
                 break;
             case 3:
                 sound = sound3
                 sound.currentTime = 25;
                 currentSong = 3;
+                sound.volume = 0.3;
+                time = 480;
                 break;
             case 4:
                 sound = sound4
                 sound.currentTime = 45;
                 currentSong = 4;
+                sound.volume = 0.1;
+                time = 480;
                 break;
             case 5:
                 sound = sound5
-                sound.currentTime = 0;
+                sound.currentTime = 32;
                 currentSong = 5;
+                sound.volume = .3;
+                time = 500;
                 break;
             case 6:
                 sound = sound6
                 sound.currentTime = 0;
                 currentSong = 6;
+                sound.volume = 0.3;
+                time = 4000;
                 break;
             case 7:
                 sound = sound7
                 sound.currentTime = 0;
                 currentSong = 7;
+                sound.volume = 0.3;
+                time = 750;
                 break;
         }
-        sound.volume = 0.3;
         sound.play();
         playing = true;
-        playButtons(buttonCycle, 500);
+        
+        playButtons(buttonCycle, time, color);
     }, 1000);
 }
 
@@ -110,7 +137,7 @@ function stopSong() {
     playing = false;
 }
 
-function playVideo() {
+function playDrumVideo() {
     drum.currentTime = 0.6;
     drum.play();
 }
@@ -118,15 +145,36 @@ function playVideo() {
 function buttonCount(num) {
     let b = getButton(num);
 
-    if (b.style.backgroundColor === "green") {
-        playVideo();
+    if (b.style.backgroundColor !== "grey") {
+        playDrumVideo();
         b.style.backgroundColor = "grey";
         buttonCounter++;
-        counter.innerText = `Beats Hit: ${buttonCounter}`
+        counter.innerText = `Beats Hit: ${buttonCounter}   `
+
+        if (buttonCounter >= 50) {
+            img4.style.display = "flex";
+            lockedImg4.style.display = "none";
+            lockedText4.style.display = "none";
+        }
+        else if (buttonCounter >= 25) {
+            img3.style.display = "flex";
+            lockedImg3.style.display = "none";
+            lockedText3.style.display = "none";
+        }
+        else if (buttonCounter >= 10) {
+            img2.style.display = "flex";
+            lockedImg2.style.display = "none";
+            lockedText2.style.display = "none";
+        }
+        if (buttonCounter >= 5) {
+            img1.style.display = "flex";
+            lockedImg1.style.display = "none";
+            lockedText1.style.display = "none";
+        }
     }
 }
 
-function playButtons(buttonCycle, timeInterval) {
+function playButtons(buttonCycle, timeInterval, color) {
     if (playing) {
         let b;
         buttonCycle++;
@@ -136,7 +184,7 @@ function playButtons(buttonCycle, timeInterval) {
         }
 
         b = getButton(buttonCycle);
-        b.style.backgroundColor = "green";
+        b.style.backgroundColor = color;
 
         setTimeout(playButtons, timeInterval, buttonCycle, timeInterval);
     }
